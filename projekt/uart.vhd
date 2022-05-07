8a561732be444e9cc706aab6eb95c667e667d49f
Josef Kuchař <josef.kuchar267@gmail.com>
2022-05-07 14:48:06 +0200

    First kinda working version

--- projekt/uart.vhd
+++ projekt/uart.vhd
@@ -1,5 +1,5 @@
 -- uart.vhd: UART controller - receiving part
--- Author(s): 
+-- Author(s): Josef Kuchař xkucha28
 --
 library ieee;
 use ieee.std_logic_1164.all;
@@ -7,17 +7,95 @@ use ieee.std_logic_unsigned.all;
 
 -------------------------------------------------
 entity UART_RX is
-port(	
-    CLK: 	    in std_logic;
-	RST: 	    in std_logic;
-	DIN: 	    in std_logic;
-	DOUT: 	    out std_logic_vector(7 downto 0);
-	DOUT_VLD: 	out std_logic
-);
-end UART_RX;  
+	port (
+		CLK : in std_logic;
+		RST : in std_logic;
+		DIN : in std_logic;
+		DOUT : out std_logic_vector(7 downto 0);
+		DOUT_VLD : out std_logic
+	);
+end UART_RX;
 
 -------------------------------------------------
 architecture behavioral of UART_RX is
+	signal cnt_bits : std_logic_vector(3 downto 0) := "0000";
+	signal cnt_clk : std_logic_vector(3 downto 0) := "0000";
+	signal rst_cnt_fsm : std_logic;
+	signal rst_cnt : std_logic;
+	signal mid_bit : std_logic;
+	signal waiting : std_logic;
+	signal reading : std_logic;
 begin
+	-- Finite state machine port mapping
+	fsm : entity work.UART_FSM(behavioral)
+		port map(
+			CLK => CLK,
+			RST => RST,
+			DIN => DIN,
+			DOUT_VLD => DOUT_VLD,
+			CNT_BITS => cnt_bits,
+			CNT_CLK => cnt_clk,
+			RST_CNT => rst_cnt_fsm,
+			WAITING => waiting,
+			READING => reading
+		);
+
+	-- Clock counter
+	p_clk_cnt : process (CLK)
+	begin
+		if rising_edge(CLK) then
+			if rst_cnt = '1' then
+				cnt_clk <= "0000";
+			else
+				cnt_clk <= cnt_clk + 1;
+			end if;
+		end if;
+	end process;
+
+	-- Bit counter
+	p_clk_bits : process (CLK)
+	begin
+		if rising_edge(CLK) then
+			if rst_cnt = '1' then
+				cnt_bits <= "0000";
+			else
+				if cnt_clk = "1111" then
+					cnt_bits <= cnt_bits + 1;
+				end if;
+			end if;
+		end if;
+	end process;
+
+	-- Reset comparator
+	p_rst_cmp : process (rst_cnt_fsm, waiting, cnt_clk)
+	begin
+		if rst_cnt_fsm = '1' or (waiting = '1' and cnt_clk = "1000") then
+			rst_cnt <= '1';
+		else
+			rst_cnt <= '0';
+		end if;
+	end process;
+
+	-- Mid bit comparator
+	p_mid_cmp : process (cnt_clk)
+	begin
+		if cnt_clk = "1111" then
+			mid_bit <= '1';
+		else
+			mid_bit <= '0';
+		end if;
+	end process;
+
+	-- Shift ouput register
+	p_shift_register : process (CLK)
+		variable dout_tmp : std_logic_vector(7 downto 0);
+	begin
+		if rising_edge(CLK) then
+			if reading = '1' and mid_bit = '1' then
+				dout_tmp := DIN & dout_tmp(7 downto 1);
+				DOUT <= dout_tmp;
+			end if;
+		end if;
+	end process;
 
 end behavioral;
--- projekt/uart_fsm.vhd
+++ projekt/uart_fsm.vhd
@@ -1,19 +1,112 @@
 -- uart_fsm.vhd: UART controller - finite state machine
--- Author(s): 
+-- Author(s): Josef Kuchař - xkucha28
 --
 library ieee;
 use ieee.std_logic_1164.all;
 
 -------------------------------------------------
 entity UART_FSM is
-port(
-   CLK : in std_logic;
-   RST : in std_logic
+   port (
+      CLK : in std_logic;
+      RST : in std_logic;
+      DIN : in std_logic;
+      CNT_BITS : in std_logic_vector(3 downto 0);
+      CNT_CLK : in std_logic_vector(3 downto 0);
+      RST_CNT : out std_logic;
+      WAITING : out std_logic;
+      READING : out std_logic;
+      DOUT_VLD : out std_logic
    );
 end entity UART_FSM;
 
 -------------------------------------------------
 architecture behavioral of UART_FSM is
+   type states is (AWAIT, START, RECIEVE, STOP, VALID);
+   signal current_state : states := AWAIT;
+   signal next_state : states := AWAIT;
 begin
+   -- State switching logic
+   p_state_switch : process (CLK, RST)
+   begin
+      if RST = '1' then
+         current_state <= AWAIT;
+      elsif rising_edge(CLK) then
+         current_state <= next_state;
+      end if;
+   end process;
 
+   -- Get next state
+   p_state_decision : process (current_state, DIN, CNT_CLK, CNT_BITS)
+   begin
+      case current_state is
+         when AWAIT =>
+            -- We found the start of start bit
+            if DIN = '0' then
+               next_state <= START;
+            end if;
+         when START =>
+            -- We are in middle of start bit
+            if CNT_CLK = "1000" then
+               -- Start bit is invalid
+               if DIN = '1' then
+                  next_state <= AWAIT;
+                  -- Start bit is valid
+               else
+                  next_state <= RECIEVE;
+               end if;
+            end if;
+         when RECIEVE =>
+            -- We read all bits
+            if CNT_BITS = "1000" then
+               next_state <= STOP;
+            end if;
+         when STOP =>
+            -- We are at the middle of stop bit
+            if CNT_CLK = "1111" then
+               -- Stop bit is valid
+               if DIN = '1' then
+                  next_state <= VALID;
+                  -- Stop bit is invalid
+               else
+                  next_state <= AWAIT;
+               end if;
+            end if;
+         when VALID =>
+            -- This is just for one clock
+            next_state <= AWAIT;
+         when others => null;
+      end case;
+   end process;
+
+   -- Output
+   p_output : process (current_state)
+   begin
+      case current_state is
+         when AWAIT =>
+            RST_CNT <= '1';
+            WAITING <= '0';
+            READING <= '0';
+            DOUT_VLD <= '0';
+         when START =>
+            RST_CNT <= '0';
+            WAITING <= '1';
+            READING <= '0';
+            DOUT_VLD <= '0';
+         when RECIEVE =>
+            RST_CNT <= '0';
+            WAITING <= '0';
+            READING <= '1';
+            DOUT_VLD <= '0';
+         when STOP =>
+            RST_CNT <= '0';
+            WAITING <= '0';
+            READING <= '0';
+            DOUT_VLD <= '0';
+         when VALID =>
+            RST_CNT <= '0';
+            WAITING <= '0';
+            READING <= '0';
+            DOUT_VLD <= '1';
+      end case;
+   end process;
 end behavioral;
